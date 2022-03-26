94f3d2b053e99797108e51cec80335c573f11ab0
lordfoom <timothys@soliditech.com>
2022-03-26 20:36:56 +0200

    Add arg stub
    Want to be able to search for words,
    but maybe I want a dsl.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(name = "RutuduFD")]
+#[clap(author = "Foom <lordfoom@gmail.com>")]
+#[clap(version = "1.0")]
+#[clap(about = "Searches through collections of rutudu lists", long_about = None)]
+struct Args{
+    #[clap(required = true, parse(from_os_str))]
+    term: Vec<String>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
