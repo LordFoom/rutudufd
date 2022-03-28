a5bc3e733c0f4e47a6bd21442b343620882501c6
lordfoom <timothys@soliditech.com>
2022-03-28 20:04:27 +0200

    Start on file scanning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::path::{Path};
+use std::fs;
 use clap::Parser;
+use color_eyre::Report;
+use color_eyre::eyre::Result;
 
 #[derive(Parser, Debug)]
 #[clap(name = "RutuduFD")]
@@ -11,7 +15,22 @@ struct Args{
     search_terms: Vec<String>,
 }
 
-fn main() {
+///Scan directory and return list or rutudu (*.rtd) files
+fn scan_directory(dir:Option<&str>) -> Result<Vec<Path>>{
+    let scan_dir = dir.unwrap_or(".");//default to the current directory
+    let result = fs::read_dir(scan_dir)?;//simple version we
+}
+/// Go through all rtd files and find matches for the search terms
+/// printing out the files and the  matches
+fn search_rtd_db_files() -> Result<(), Report> {
+
+
+    //scan the  files
+    Ok(())
+}
+
+fn main()->Result<()> {
+    color_eyre::install()?;
 
     let args = Args::parse();
 
